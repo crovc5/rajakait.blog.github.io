dd1f5d31c53bd5971615361b88eeb91b0053342e
crovc5 <59214423+crovc5@users.noreply.github.com>
2020-02-25 22:26:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,43 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8"><meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
-      <title>SMS SEND</title>
-</head>
-<body>
-    <form method="post">
-        <table align="center" border="1">
-            <tr>
-                <td>Number</td>
-                <td><input type="text" name="number"></td>
-            </tr>
-            <tr>
-                <td colspan="2" align="center"><input type="submit" name="submit" value="Send"></td>
-            </tr>
-        </table>
-    </form>
-</body>
-</html>
-<?php
-if(isset($_POST['submit']))
-{
-    $number = $_POST['number'];
-    
-$curl = curl_init();
-
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://www.fast2sms.com/dev/bulk"));
-
-$response = curl_exec($curl);
-$err = curl_error($curl);
-
-curl_close($curl);
-
-if ($err) {
-  echo "cURL Error #:" . $err;
-} else {
-  echo $response;
+<style>
+.card-tool .input-text {
+	 outline:none;
+	 border: 0px solid #d3d3d3;
+	 width: 100%;
+	 height: 100px;
+	 padding: 10px;
+	 border: 2px solid #d3d3d3;
+	 color: #111;
+	 transition: 0.5s;
 }
+.card-tool input[type=submit] {
+	 background: #005cff;
+	 border: none;
+	 width: 30%;
+	 height: 25px;
+	 box-shadow: 0 5px 10px 0 rgba(0, 0, 0, 0.3), 0 7px 21px 0 rgba(0, 0, 0, 0.20);
+	 color: #fff;
+	 font-size: 15px;
+	 margin: 5px;
+	 padding: 0 5px 0 5px;
+	 float: right;
+	 outline: none;
+	 border-radius: 20px;
 }
-
-?>
+ </style>
+<div class="card card-body card-tool mb-5">
+	<div class="text-center" id="result"></div>
+	<form method="get" autocomplete="off">
+	<textarea class="input-text rounded-lg" placeholder="Enter your address" name="address" required="required"></textarea>
+	<input type="submit">
+</form>
+</div>
